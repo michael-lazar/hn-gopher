ff27953e5cdaf79fa810a635fbecc304c71f1188
Michael Lazar <michael.lazar.ctr@dren.hpc.mil>
2017-06-16 01:32:43 -0400

    Cleaning up the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,19 @@
+#!/bin/bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+apt-get install -y curl lynx vim
 apt-get install -y python3 python3-requests python3-lxml python3-unidecode
-apt-get instal -y libwrap0-dev
-cp -R $DIR/hn-gopher/opt/gophernicus_* /opt/
-
-pushd /opt/gophernicus_*
-make
-make install
-make clean
-popd
-
-# Don't want to delete the archive/live folders
-# rm -rf /var/gopher
+apt-get install -y libwrap0-dev
 
 cp -R $DIR/hn-gopher/var/gopher /var/gopher
 cp -R $DIR/hn-gopher/bin/* /usr/local/bin/
 cp -R $DIR/hn-gopher/etc/cron.d/* /etc/cron.d/
+cp -R $DIR/hn-gopher/opt/gophernicus_* /opt/
 
-rm /etc/default/gophernicus
+pushd /opt/gophernicus_*
+make && make install && make clean-build
+popd
+
+rm -f /etc/default/gophernicus
 cp $DIR/hn-gopher/opt/gophernicus_*/gophernicus.env /etc/default/gophernicus
