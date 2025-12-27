b50bc390abea202cd72bd5922b5b457b22478c25
Michael Lazar <lazar.michael22@gmail.com>
2025-12-26 23:40:01 -0500

    Trying to figure out permissions

--- vendor/gophernicus_2.4/gophernicus.c
+++ vendor/gophernicus_2.4/gophernicus.c
@@ -707,8 +707,8 @@ get_selector:
 	/* Everyone must have read access but no write access */
 	if ((file.st_mode & S_IROTH) == 0)
 		die(&st, ERR_ACCESS, "File or directory not world-readable");
-	if ((file.st_mode & S_IWOTH) != 0)
-		die(&st, ERR_ACCESS, "File or directory world-writeable");
+//	if ((file.st_mode & S_IWOTH) != 0)
+//		die(&st, ERR_ACCESS, "File or directory world-writeable");
 
 	/* If stat said it was a dir then it's a menu */
 	if ((file.st_mode & S_IFMT) == S_IFDIR) st.req_filetype = TYPE_MENU;
